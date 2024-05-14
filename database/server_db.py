d240e053f365405c9c11d0307044fe8b965dfde8
Your Name <kasper_322@mail.ru>
2024-05-14 21:54:23 +0300

    for 14 05 2024 part 2

--- database/server_db.py
+++ database/server_db.py
@@ -1,22 +1,28 @@
 import asyncio
 import os
-import psycopg2
 import asyncpg
 from dotenv import load_dotenv
 load_dotenv()
 
 
 print(os.getenv('DB_HOST'))
-async def get_user_to_db(user_id: int):
+
+async def create_pool():
     db_server_pool = await asyncpg.create_pool(database=os.getenv('DB_SERV_NAME'),
-                        host=os.getenv('DB_HOST'),
-                        port=os.getenv('DB_PORT'),
-                        user=os.getenv('DB_USER'),
-                        password=os.getenv('DB_PAS'))
+                                               host=os.getenv('DB_HOST'),
+                                               port=os.getenv('DB_PORT'),
+                                               user=os.getenv('DB_USER'),
+                                               password=os.getenv('DB_PAS'))
+    return db_server_pool
+
+
+async def get_user_to_db(user_id: int):
+    db_server_pool = await create_pool()
     async with db_server_pool.acquire() as conn:
         query = f"SELECT * FROM telegram_users"
         result = await conn.fetch(query)
         print(result[0]['account_id'])
         await conn.close()
+
 if __name__ == "__main__":
     asyncio.run(get_user_to_db(734434528))
--- main.py
+++ main.py
@@ -13,19 +13,16 @@ import asyncpg
 load_dotenv()
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher()
-BANK_DATABASE_URL = os.getenv('BANK_DATABASE_URL')
-SERVER_DATABASE_URL = os.getenv('SERVER_DATABASE_URL')
+# BANK_DATABASE_URL = os.getenv('BANK_DATABASE_URL')
+# SERVER_DATABASE_URL = os.getenv('SERVER_DATABASE_URL')
 dp.include_routers(admin_router,
                    router)
-global db_bank_pool, db_server_pool
 
 logging.basicConfig(level=logging.INFO)
 
 async def handler(event, context):
     body = json.loads(event['body'])
     update = types.Update.parse_obj(body)
-    db_bank_pool = await asyncpg.create_pool(BANK_DATABASE_URL)
-    db_server_pool = await asyncpg.create_pool(SERVER_DATABASE_URL)
     await dp.feed_webhook_update(bot, update)
 
 
